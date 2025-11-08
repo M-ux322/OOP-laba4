015792ec9c0cb60fa3254f4a8e4446a12dcfa7f6
epepei <manya.ermakova@gmail.com>
2025-11-08 22:27:26 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -4,7 +4,6 @@
 
 template<typename T>
 concept Scalar = std::is_scalar_v<T>;
-
 template<Scalar T>
 class Point {
 private:
@@ -12,14 +11,11 @@ private:
 public:
     Point() : x_(0), y_(0) {}
     Point(T x, T y) : x_(x), y_(y) {}
-    
     T x() const { return x_; }
     T y() const { return y_; }
-    
     bool operator==(const Point<T>& other) const {
         return x_ == other.x_ && y_ == other.y_;
     }
-    
     friend std::ostream& operator<<(std::ostream& os, const Point<T>& point) {
         os << "(" << point.x_ << ", " << point.y_ << ")";
         return os;
