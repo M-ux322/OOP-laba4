bab6f6a9ad918a457ec52268fed11a458a2bac77
epepei <manya.ermakova@gmail.com>
2025-11-08 22:26:34 +0300

    Update Figure.h

--- Figure.h
+++ Figure.h
@@ -11,8 +11,6 @@ public:
     virtual double area() const = 0;
     virtual void print_vert() const = 0;
     virtual std::unique_ptr<Figure<T>> copy() const = 0;
-    
     operator double() const { return area(); }
-    
     virtual bool operator==(const Figure<T>& other) const = 0;
 };
