3b238610600097300cd0ae2729f04881401274da
epepei <manya.ermakova@gmail.com>
2025-11-08 22:28:07 +0300

    Update Polygon.cpp

--- Polygon.cpp
+++ Polygon.cpp
@@ -4,17 +4,15 @@
 
 template<Scalar T>
 Polygon<T>::Polygon() {}
-
 template<Scalar T>
+
 Polygon<T>::Polygon(const std::vector<Point<T>>& vertices) : vertices_(vertices) {
     if (vertices.size() < 3) throw "At least 3 vertices required";
 }
-
 template<Scalar T>
 Polygon<T>::Polygon(std::initializer_list<Point<T>> verts) : vertices_(verts) {
     if (verts.size() < 3) throw "At least 3 vertices required";
 }
-
 template<Scalar T>
 Point<T> Polygon<T>::geom_center() const {
     T sum_x = 0, sum_y = 0;
@@ -24,7 +22,6 @@ Point<T> Polygon<T>::geom_center() const {
     }
     return Point<T>(sum_x / vertices_.size(), sum_y / vertices_.size());
 }
-
 template<Scalar T>
 double Polygon<T>::area() const {
     if (vertices_.size() < 3) return 0.0;
@@ -37,7 +34,6 @@ double Polygon<T>::area() const {
     }
     return std::abs(sum1 - sum2) / 2.0;
 }
-
 template<Scalar T>
 void Polygon<T>::print_vert() const {
     std::cout << "Polygon vertices: ";
@@ -47,16 +43,13 @@ void Polygon<T>::print_vert() const {
     }
     std::cout << std::endl;
 }
-
 template<Scalar T>
 std::unique_ptr<Figure<T>> Polygon<T>::copy() const {
     return std::make_unique<Polygon<T>>(*this);
 }
-
 template<Scalar T>
 bool Polygon<T>::operator==(const Figure<T>& other) const {
     auto* p = dynamic_cast<const Polygon<T>*>(&other);
     return p && vertices_ == p->vertices_;
 }
-
 template class Polygon<double>;
